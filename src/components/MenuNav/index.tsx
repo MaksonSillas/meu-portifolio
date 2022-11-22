280f0582f1d9a277cfb7a748a51a613a27c437cd
Makson <maksonsantoss4@gmail.com>
2022-11-22 12:54:35 -0300

    feat: portifólio v3.0

--- src/components/MenuNav/index.tsx
+++ src/components/MenuNav/index.tsx
@@ -1,23 +1,32 @@
 import { DivContainerNav } from "./style";
+import { ReactNode, useEffect, useRef } from "react";
 
-export function MenuNav() {
+interface IMenuNav {
+  children: ReactNode;
+  setNavIsOpen: (item: boolean) => void;
+}
+
+export function MenuNav({ children, setNavIsOpen }: IMenuNav) {
+  const navRef = useRef<HTMLElement>(null);
+
+  useEffect(() => {
+    function handleOutClick(e: MouseEvent) {
+      if (!navRef.current?.contains(e.target as Node)) {
+        setNavIsOpen(false);
+      }
+    }
+
+    document.addEventListener("mousedown", handleOutClick);
+
+    return () => {
+      document.removeEventListener("mousedown", handleOutClick);
+    };
+  }, []);
+
   return (
     <DivContainerNav>
-      <nav>
-        <ul>
-          <li>
-            <a href="#">Home</a>
-          </li>
-          <li>
-            <a href="#skills">Habilidades</a>
-          </li>
-          <li>
-            <a href="#projects">Projetos</a>
-          </li>
-          <li>
-            <a href="#contacts">Contatos</a>
-          </li>
-        </ul>
+      <nav className="nav-box" ref={navRef}>
+        {children}
       </nav>
     </DivContainerNav>
   );
